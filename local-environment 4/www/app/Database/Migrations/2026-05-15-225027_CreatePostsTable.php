bb797e4e9651a1e7194b6a4404d6bdc0f4b9dbb7
Oliwia <oliwia.wojcicka05@gmail.com>
2026-05-16 00:53:58 +0200

    Add posts table migration

---
+++ local-environment 4/www/app/Database/Migrations/2026-05-15-225027_CreatePostsTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+            ],
+            'content' => [
+                'type' => 'TEXT',
+                'null' => false,
+            ],
+            'image' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('user_id');
+
+        $this->forge->createTable('posts');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('posts');
+    }
+}
